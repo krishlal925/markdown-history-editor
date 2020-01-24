61d0e4a78e3ef405559438869e871fcd468ad9a3
Neel <krishlal925@gmail.com>
2020-01-23 21:09:09 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Marked from 'marked';
 import './App.css';
 
 function App() {
+  const [text, setText]= useState('')
+  let test = Marked(text);
+  console.log(test)
+  function updateText(ev){
+    console.log(ev.target)
+    setText(ev.target.value);
+  }
+  function createMarkup(){
+    return {__html: Marked(text)};
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className= "box">
+        <textarea rows="10" value = {text} onChange= {updateText}/>
+      </div >
+      <div className= "box" dangerouslySetInnerHTML = {createMarkup()}>
+
+      </div>
+      <div>
+        <button>Save</button>
+      </div>
     </div>
   );
 }
